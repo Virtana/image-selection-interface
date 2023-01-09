9b2d335483904be014b638f29db8c8b58338e06b
raeanne <raeanne@virtanatech.com>
2023-01-08 22:33:17 -0400

    authenticating users

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ function loadPage() {
     window.location="photoviewer.html";
 }
 
+// Set region
+AWS.config.region = 'us-east-2';
+
 function saveCreds(creds){
   localStorage.setItem("creds", creds);
-  loadPage();
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: creds
+  });
+
+  AWS.config.getCredentials(function(err) {
+    if (err){
+      alert("Invalid credentials!");
+    }
+    else {
+      loadPage();
+    }
+  });
 }
